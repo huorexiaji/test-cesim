34286a1e291166e5c4a218c762868106c2fc23dd
Hou Kunxiao <khou@accel.local>
2023-12-07 17:26:18 +0800

    create project

---
+++ src/App.vue
@@ -0,0 +1,147 @@
+<script setup>
+import { defineComponent, ref, watch, computed  } from 'vue';
+import { MailOutlined, QqOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
+
+import home from './view/home.vue';
+import user from './view/user.vue';
+import cesium from './view/cesium.vue'
+
+const routes = {
+  '/': home,
+  '/user': user,
+  '/cesium': cesium
+}
+const currentPath = ref(window.location.hash)
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || home
+})
+
+const selectedKeys = ref(['1']);
+const openKeys = ref(['sub1']);
+const handleClick = e => {
+  console.log('click', e);
+};
+const titleClick = e => {
+  console.log('titleClick', e);
+};
+watch(
+  () => openKeys,
+  val => {
+    console.log('openKeys', val);
+  },
+);
+
+</script>
+
+<template>
+  <a-layout style="min-width: 1920px;">
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys1"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">nav 1</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu
+          mode="inline"
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          :style="{ height: '100%', borderRight: 0 }"
+        >
+          <a-sub-menu key="sub1">
+            <template #title>
+              <span>
+                <user-outlined />
+                subnav 1
+              </span>
+            </template>
+            <a-menu-item key="1"><a href="#/">Home</a></a-menu-item>
+            <a-menu-item key="2"><a href="#/user">User</a></a-menu-item>
+            <a-menu-item key="3"><a href="#/cesium">Cesium</a></a-menu-item>
+            <a-menu-item key="4">option4</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <template #title>
+              <span>
+                <laptop-outlined />
+                subnav 2
+              </span>
+            </template>
+            <a-menu-item key="5">option5</a-menu-item>
+            <a-menu-item key="6">option6</a-menu-item>
+            <a-menu-item key="7">option7</a-menu-item>
+            <a-menu-item key="8">option8</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub3">
+            <template #title>
+              <span>
+                <notification-outlined />
+                subnav 3
+              </span>
+            </template>
+            <a-menu-item key="9">option9</a-menu-item>
+            <a-menu-item key="10">option10</a-menu-item>
+            <a-menu-item key="11">option11</a-menu-item>
+            <a-menu-item key="12">option12</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+        <component :is="currentView" />
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}</style>
---
+++ src/view/cesium.vue
@@ -0,0 +1,19 @@
+<template>
+    <div ref="cesiumContainer"></div>
+</template>
+
+<script setup>
+import { ref, onMounted  } from 'vue';
+import { Viewer, Ion } from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+const cesiumContainer = ref()
+window.CESIUM_BASE_URL = '/Cesium'
+
+onMounted(()=>{
+    Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxMzVkMmNjMi1hZmM3LTQ5YmEtYWUxMC1kZGI4ZGQ2MmQyMTYiLCJpZCI6MTgyNTgwLCJpYXQiOjE3MDE3NjU4NjB9.9B8RBQW__XK22qbDtKnwT4fmy6XBWu08Zk3fEx0dPzc'
+    const viewer = new Viewer(cesiumContainer.value);
+})
+
+</script>
+
